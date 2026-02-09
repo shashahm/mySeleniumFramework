1fd7a41ed5b3da6e6d30a044d22e1141fe416c18
Ahmed <ahmedhshash@outlook.com>
2026-02-09 03:47:16 +0100

    allow parralel execution

--- src/test/java/runners/MainRunner.java
+++ src/test/java/runners/MainRunner.java
@@ -2,6 +2,7 @@ package runners;
 
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
+import org.testng.annotations.DataProvider;
 
 @CucumberOptions(features = {"classpath:features"},
         glue = {"stepDefinitions"},
@@ -11,4 +12,10 @@ import io.cucumber.testng.CucumberOptions;
         plugin = {"pretty","html:target/cucumber_report.html","json:target/cucumber_report.json"}
 )
 public class MainRunner extends AbstractTestNGCucumberTests {
+
+    @Override
+    @DataProvider(parallel = true)
+    public Object[][] scenarios() {
+        return super.scenarios();
+    }
 }
