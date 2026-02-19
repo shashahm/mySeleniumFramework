1aa38e987448871b20e26c817e87a92f7504ef54
Ahmed <ahmedhshash@outlook.com>
2026-02-20 00:44:57 +0100

    Adding image capture when scenario fails

--- src/test/java/stepDefinitions/base/Hooks.java
+++ src/test/java/stepDefinitions/base/Hooks.java
@@ -1,6 +1,13 @@
 package stepDefinitions.base;
 import io.cucumber.java.After;
+import io.cucumber.java.AfterStep;
 import io.cucumber.java.Before;
+import io.cucumber.java.Scenario;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+
+import java.sql.Timestamp;
+
 import static driver.DriverFactory.cleanUpDriver;
 import static driver.DriverFactory.getDriver;
 
@@ -12,9 +19,24 @@ public class Hooks {
         getDriver();
     }
 
+    @AfterStep
+    public void captureExceptionImage(Scenario scenario){
+        if (scenario.isFailed()){
+            Timestamp timestamp = new Timestamp(System.currentTimeMillis());
+            String timeMilliSeconds = Long.toString(timestamp.getTime());
+
+            byte[] screenshot = ((TakesScreenshot) getDriver()).getScreenshotAs(OutputType.BYTES);
+
+        scenario.attach(screenshot, "image/png", timeMilliSeconds);
+
+        }
+    }
+
     @After
     public void tearDown(){
         cleanUpDriver();
     }
+
+
 
 }
